35bfcd5f519961038c45a3dc4e696e3e9bdf6f5f
samuel <s70426546@gmail.com>
2025-01-22 22:09:09 -0500

    Page Danger no dates

--- app/src/main/java/com/example/tsasoftwaredev/Dates.java
+++ app/src/main/java/com/example/tsasoftwaredev/Dates.java
@@ -1,8 +1,6 @@
 package com.example.tsasoftwaredev;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class DangerNearMe extends AppCompatActivity {
+public class Dates extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_danger_near_me);
+        setContentView(R.layout.activity_dates);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void launchRecent(View v){
-        //launchDangerNearME
-        Intent r = new Intent(this, Dates.class);
-        startActivity(r);
-    }
-    public void launchAllTime(View v) {
-        //launchDangerNearME
-        Intent a = new Intent(this, Dates.class);
-        startActivity(a);
     }
 }
